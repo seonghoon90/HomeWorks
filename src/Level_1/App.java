ad34f9efa7fa39e1a3a17d9ea35abd7d6e019d96
hoon <dotofi@naver.com>
2024-04-30 20:36:33 +0900

    Level_1 6번

--- src/Level_1/App.java
+++ src/Level_1/App.java
@@ -1,14 +1,16 @@
 package Level_1;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] results = new int[10];
+        Queue<Integer> results = new LinkedList<>();
         int count = 0;
 
-            while (count < 10) { //반복문
+            while (true) { //반복문
             System.out.println("첫 번째 숫자를 입력하세요: ");
             // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
             int a = sc.nextInt();
@@ -49,8 +51,11 @@ public class App {
                     valid = false;
             }
             if (valid) {
-                results[count] = result;
-                count++;
+                if (results.size() == 10){
+                    results.poll();
+                } else {
+                    results.offer(result);
+                }
                 System.out.println("결과: " + result); // 최종 결과 출력
 
             }
@@ -62,6 +67,5 @@ public class App {
                     break;
                 }
         }
-
     }
 }
