46b787d753a082e7fa7c4b7395fc448199e7ed43
hoon <dotofi@naver.com>
2024-04-30 12:20:37 +0900

    Level_1 1번

---
+++ src/Level_1/App.java
@@ -0,0 +1,17 @@
+package Level_1;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        int n = sc.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        int m = sc.nextInt();
+    }
+}
